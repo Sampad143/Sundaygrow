1c6b8b74e2129b5f8a2369813a49881c7a45bf2e
sampadbiswas <sampadbiswas143@gmail.com>
2025-10-24 16:22:10 +0600

    Initial commit: Sundaygrow project setup

--- src/main.js
+++ src/main.js
@@ -1,29 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// SCSS
+import './scss/main.scss';
 
+// Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Custom JS
+import './js/app.js';
+
+// App HTML
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello Sundaygrow!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="counter" type="button">Click Me</button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Your project is ready.
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+// Example counter function
+import { setupCounter } from './counter.js';
+setupCounter(document.querySelector('#counter'));
 
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './scss/main.scss';
-import './js/app.js';
+console.log('Sundaygrow Project Loaded!');
