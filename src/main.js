7502d594d949a3adce4892bfdaf6c0861d630b39
sampadbiswas <sampadbiswas143@gmail.com>
2025-11-07 07:05:23 +0600

    #

--- src/main.js
+++ src/main.js
@@ -7,21 +7,4 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 // Custom JS
 import './js/app.js';
 
-// App HTML
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Sundaygrow!</h1>
-    <div class="card">
-      <button id="counter" type="button">Click Me</button>
-    </div>
-    <p class="read-the-docs">
-      Your project is ready.
-    </p>
-  </div>
-`;
 
-// Example counter function
-import { setupCounter } from './counter.js';
-setupCounter(document.querySelector('#counter'));
-
-console.log('Sundaygrow Project Loaded!');
